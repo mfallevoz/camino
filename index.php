145bc33b47dba220fac026188a7077f91229431d
Melchior Fallevoz <muzikere@PingBook-Pro.local>
2022-06-11 18:18:30 +0200

    add(front): dashboard

--- index.php
+++ index.php
@@ -9,8 +9,62 @@
   </head>
   <body>
     <h1>Panneau de contrôle</h1>
-    <div class="card">
-
+    <div class="container">
+      <div class="cards">
+        <div class="card">
+          <h2>Item 1</h2>
+          <div class="title">
+            <div class="changeTitle">
+              Changer titre
+            </div>
+            <input type="text" id="title" name="title" placeholder="titre">
+          </div>
+  
+          <div class="toShow">
+            Afficher l'item 1
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span>
+            </label>
+          </div>
+        </div>
+  
+        <div class="card">
+          <h2>Item 2</h2>
+          <div class="title">
+            <div class="changeTitle">
+              Changer titre
+            </div>
+            <input type="text" id="title" name="title" placeholder="titre">
+          </div>
+  
+          <div class="toShow">
+            Afficher l'item 2
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span>
+            </label>
+          </div>
+        </div>
+  
+        <div class="card">
+          <h2>Item 3</h2>
+          <div class="title">
+            <div class="changeTitle">
+              Changer titre
+            </div>
+            <input type="text" id="title" name="title" placeholder="titre">
+          </div>
+  
+          <div class="toShow">
+            Afficher l'item 3
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   </body>
 </html>
